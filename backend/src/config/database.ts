cbe6f7c30115b8aba97f78cfb7ba9a12143ab729
KevinFMfatec <mirendakevin@gmail.com>
2024-06-13 21:54:13 -0300

    Configurações para conectar no banco de dados

---
+++ backend/src/config/database.ts
@@ -0,0 +1,20 @@
+import "reflect-metadata";
+import '../config/dotenv';
+
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+  type: "mysql",
+  host: process.env.HOST,
+  port: Number(process.env.PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DATABASE,
+  synchronize: true,
+  logging: false,
+  entities: [
+    "src/models/*.ts"
+  ],
+  migrations: [],
+  subscribers: []
+})
